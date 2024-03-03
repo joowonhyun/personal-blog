d5d6c29bbc209d200e975187e57adcdb67331a3e
Joowon Hyun <lkk7913@naver.com>
2024-03-03 22:22:37 +0900

    style : postPreview page

--- components/post/PostPreview.tsx
+++ components/post/PostPreview.tsx
@@ -1,18 +1,14 @@
 import Link from "next/link";
 import { PostMetadata } from "./PostMetadata";
-
+import styles from './PostPreview.module.css'
 const PostPreview = (props: PostMetadata) => {
   return (
-    <div
-      className="border border-slate-300 p-4 rounded-md shadow-sm
-    bg-white"
-    >
-      <p className="text-sm text-slate-400">{props.date}</p>
-
+    <div>
+      <p className={styles.date}>{props.date}</p>
       <Link href={`/posts/${props.slug}`}>
-        <h2 className=" text-violet-600 hover:underline mb-4">{props.title}</h2>
+        <h2 className={styles.title}>{props.title}</h2>
       </Link>
-      <p className="text-slate-700">{props.subtitle}</p>
+      <p className={styles.subTitle}>{props.subtitle}</p>
     </div>
   );
 };
