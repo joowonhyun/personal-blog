430e664414e6ef83ca7cecbea56e42117e541db8
Joowon Hyun <lkk7913@naver.com>
2024-03-24 20:44:53 +0900

    revert build code

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,8 +2,6 @@ import styles from "./posts.module.css";
 import getPostMetadata from "../../components/Post/getPostMetadata";
 import PostPreview from '../../components/Post/previewPost';
 
-export const dynamic = 'force-dynamic'
-
 const HomePage = () => {
   const postMetadata = getPostMetadata();
   const postPreviews = postMetadata.map((post) => (
