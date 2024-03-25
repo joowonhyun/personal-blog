0a41bed78624e6bbb2f72aa88a159751c6fc38f0
Joowon Hyun <lkk7913@naver.com>
2024-03-25 23:15:43 +0900

    fix : add async

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,20 +1,20 @@
-import fs from "fs";
+import { promises as fs } from 'fs';
 import Markdown from "markdown-to-jsx";
 import matter from "gray-matter";
 import styles from "./post.module.css";
 import CodeBlock from "@/posts/markdown/CodeBlock";
 
-const getPostContent = (slug: string) => {
-  const folder = "posts";
-  const file = `${folder}/${slug}.md`;
-  const content = fs.readFileSync(file, "utf8");
+const getPostContent = async (slug: string) => {
+  const file = `posts/${slug}.md`;
+  const content = await fs.readFile(process.cwd() + file, "utf8");
   const matterResult = matter(content);
   return matterResult;
 };
 
-const PostPage = (props: any) => {
+const PostPage = async (props: any) => {
   const slug = props.params.slug;
-  const post = getPostContent(slug);
+  const post = await getPostContent(slug);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>{post.data.title}</h1>
